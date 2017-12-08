d03f03bec713d97755b3720256bf9a4ed9b0fe58
Ryu955 <c0114046aa@edu.teu.ac.jp>
2017-12-08 17:35:12 +0900

    Update 岩が動いた

---
+++ project/Assets/Script/IwaController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IwaController : MonoBehaviour {
+
+	float rotSpeed;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		this.transform.Translate (-0.1f, 0, 0);
+		if (transform.position.x < -6.5f) {
+			Destroy (this.gameObject);
+		}
+	}
+
+}
---
+++ project/Assets/Script/IwaGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IwaGenerator : MonoBehaviour {
+
+	public GameObject[] rocks = new GameObject[3];
+	private int num;
+
+	// Use this for initialization
+	void Start () {
+		 num = Random.Range (0, rocks.Length);
+		InvokeRepeating ("GenRock", 1, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		num = Random.Range (0, rocks.Length);
+	}
+
+	void GenRock () {
+//		Instantiate (GameObject[num], new Vector3 (-2.5f + 5 * Random.value, 6, 0), transform.rotation);
+		Instantiate(rocks[num],new Vector3 (10f, -5f, -1f),transform.rotation);
+	}
+}
